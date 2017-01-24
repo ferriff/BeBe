6aadc6258eae1bac7973dc56f5489f92a1e2e4c5
Federico Ferri <federico.ferri@cern.ch>
2017-01-24 15:51:44 +0100

    improve the analysis example

--- bin/quickana.cc
+++ bin/quickana.cc
@@ -1,7 +1,8 @@
 #include "pulse.h"
 
-#include <iostream>
+#include <math.h>
 #include <fstream>
+#include <iostream>
 
 #include "TF1.h"
 #include "TFile.h"
@@ -31,12 +32,6 @@ void set_branches(TTree * t)
         TLeaf * l = t->GetLeaf("nsamples", "nsamples");
         _e.data = (bb::daqint_t *)calloc(l->GetMaximum(), sizeof(bb::daqint_t));
         t->SetBranchAddress("raw_pulse", _e.data);
-        /////t->SetBranchAddress("amplitude", &_e.ampl);
-        /////t->SetBranchAddress("baseline", &_e.bl);
-        /////t->SetBranchAddress("t_max", &_e.t_max);
-        /////t->SetBranchAddress("t_max_daq", &_e.t_max_daq);
-        /////t->SetBranchAddress("t_start_daq", &_e.t_start_daq);
-        /////t->SetBranchAddress("pu", &_e.pu);
         t->SetBranchAddress("nsamples", &_e.nsamples);
         t->SetBranchAddress("detid", &_e.detid);
         t->SetBranchAddress("time", &_e.ts);
@@ -53,12 +48,15 @@ int main()
         TH1F     * h_ampl_heater            = (TH1F*)h_ampl_raw->Clone("h_ampl_heater");
         TH1F     * h_ampl_heater_drift_corr = (TH1F*)h_ampl_raw->Clone("h_ampl_heater_drift_corr");
         TH1F     * h_ampl_keV               = new TH1F("h_ampl_keV", "h_ampl_keV", 4000, 0., 10000.);
+        TH1F     * h_ampl_res               = new TH1F("h_ampl_res", "h_ampl_res", 1000, -10., 10.);
         TH1F     * h_ped                    = new TH1F("h_ped", "h_ped", 2<<13, 0., 2<<17);
         TH1F     * h_ped_raw                = new TH1F("h_ped_raw", "h_ped_raw", 1000, -1e5, 1e5);
         TH1F     * h_ped_rms                = new TH1F("h_ped_rms", "h_ped_rms", 100, 0., 100.);
         TH1F     * h_dt                     = new TH1F("h_dt", "h_dt", 5000, 0., 5000000.);
         TH1F     * h_time_s1                = new TH1F("h_time_s1", "h_time_s1", 20000, 0., 50.);
-        TProfile * p_average_signal         = new TProfile("p_average_signal", "p_average_signal", 5. * TIME_WINDOW, 0., TIME_WINDOW);
+        TGraph   * g_decay_vs_rise          = new TGraph();
+        g_decay_vs_rise->SetNameTitle("g_decay_vs_rise", "g_decay_vs_rise");
+        gDirectory->Add(g_decay_vs_rise);
         TGraph * g_baseline_vs_t            = new TGraph();
         g_baseline_vs_t->SetNameTitle("g_baseline_vs_t", "g_baseline_vs_t");
         gDirectory->Add(g_baseline_vs_t);
@@ -76,11 +74,10 @@ int main()
         fout->cd();
         set_branches(t);
         Long64_t nentries = t->GetEntries();
-        Long64_t gcnt = 0, gcnt2 = 0, gcnta = 0;
+        Long64_t gcnt = 0, gcnt2 = 0, gcnta = 0, gcnt_rd = 0;
         UInt_t otmaxdaq = 1;
-        float fata[TIME_WINDOW]; // FIXME
         FILE * fp = fopen("pulses.dat", "w");
-        size_t ipulse = 1, totpulse = 1;
+        size_t ipulse = 0, totpulse = 1;
         std::ofstream ofs;
         ofs.open ("pappa.dat", std::ofstream::out);
         for (Long64_t ien = 0; ien < nentries; ++ien) {
@@ -108,8 +105,9 @@ int main()
                         ofs.flush();
                         // pause until Enter key pressed
                         std::cerr << "pulse " << ipulse << " of " << totpulse << " dumped, press [Enter] to continue...\n";
-                        //getchar();
+                        //getchar(); // uncomment if you want to pause
                         ++ipulse;
+                        continue; // do not perform the rest of the analysis
                 }
                 auto res = p.maximum(100, p.n_samples());
                 size_t iM = res.first;
@@ -118,51 +116,40 @@ int main()
                 float fiM = res.first;
                 float fM = res.second;
                 h_ampl->Fill(fM);
-                std::cerr << "--> " << iM << " " << M << " " << fiM << " " << fM << "\n";
-                float ft_daq = _e.ts + fiM;
+                h_ampl_res->Fill((fM - M) / fM);
+                //std::cerr << "--> " << iM << " " << M << " " << fiM << " " << fM << "\n";
+                float ft_daq = _e.ts + fiM; // in seconds
                 float trise = p.rise_time_interpolated(iM, 0.05, fM, fiM) - p.rise_time_interpolated(iM, 0.95, fM, fiM);
+                float tdecay = p.decay_time_interpolated(iM, 0.20, fM, fiM) - p.decay_time_interpolated(iM, 0.95, fM, fiM);
+                if(!(isnan(trise) || isnan(tdecay))) {
+                        g_decay_vs_rise->SetPoint(gcnt_rd++, trise, tdecay);
+                }
                 /*
-                float tdecay = signal_decay_time_interpolated(fata, iM, 0.20, fM, fiM) - signal_decay_time_interpolated(fata, iM, 0.95, fM, fiM);
                 fprintf(stdout, "%f %f %lu %u %u %lu %lu %f %f %f %f %f %f\n",
                         _e.ampl, M, iM, _e.t_max, _e.t_max_daq, 
                         signal_rise_time(fata, iM, 0.05), signal_decay_time(fata, iM, 0.20),
                         trise,
                         tdecay, 
                         fM, fiM, ped, ped_rms);
+                */
                 float adc2keV = 4783. / 101279.;
                 h_ampl_keV->Fill(fM * adc2keV);
-                h_ampl_drift_corr->Fill(fM - 1.788 * ft_daq / 2000. / 3600.);
+                h_ampl_drift_corr->Fill(fM - 1.788 * ft_daq / 3600.);
                 //if (otmaxdaq && _e.ampl > 101100 && _e.ampl < 101500) {
-                if (otmaxdaq && _e.ampl > 46400 && _e.ampl < 46500) {
-                        h_dt->Fill(_e.t_max_daq - otmaxdaq);
+                if (otmaxdaq && fM > 46400 && fM < 46500) {
+                        h_dt->Fill(_e.ts - otmaxdaq);
                         h_ampl_heater->Fill(fM);
-                        h_ampl_heater_drift_corr->Fill(fM - 1.788 * ft_daq / 2000. / 3600.);
-                        otmaxdaq = _e.t_max_daq;
+                        h_ampl_heater_drift_corr->Fill(fM - 1.788 * ft_daq / 3600.);
+                        otmaxdaq = _e.ts;
                 }
-                if (_e.ampl > 46400 && _e.ampl < 46500) {
-                        g_ampl_vs_t->SetPoint(gcnt++, ft_daq / 2000. / 3600., fM);
-                } else if (_e.ampl > 100.8e+3 && _e.ampl < 101.6e+3) {
-                        g_ampl2_vs_t->SetPoint(gcnt2++, ft_daq / 2000. / 3600., fM);
+                if (fM > 46400 && fM < 46500) {
+                        g_ampl_vs_t->SetPoint(gcnt++, ft_daq / 3600., fM);
+                } else if (fM > 100.8e+3 && fM < 101.6e+3) {
+                        g_ampl2_vs_t->SetPoint(gcnt2++, ft_daq / 3600., fM);
                 }
-                g_ampl_all_vs_t->SetPoint(gcnta++, (float)_e.t_start_daq / 2000. / 3600., fM);
+                g_ampl_all_vs_t->SetPoint(gcnta++, ft_daq / 3600., fM);
                 if (fM > 1e5) h_time_s1->Fill(trise - 0.01 * fM / 1000.);
                 //else if (_e.ampl > 101.2e+3 && _e.ampl < 101.4e+3) g_ampl2_vs_t->SetPoint(gcnt2++, ft_daq / 2000. / 3600., fM);
-                int flag = signal_flag(fM, trise, tdecay, ped_rms);
-                if (fM > 1e4) {
-                        fprintf(fp, "#flag: %d\n", flag);
-                        for (size_t i = 0; i < TIME_WINDOW; ++i) {
-                                fprintf(fp, "%lu %f %f %f\n", i, 450 - fiM, fata[i], fM);
-                        }
-                        fprintf(fp, "\n\n");
-                }
-                if (_e.pu == 0 && fM > 101e+3 && fM < 101.75e+3 && ien > 750 && ien < 1000) {
-                        for (size_t is = 0; is < TIME_WINDOW; ++is) {
-                                //fprintf(stderr, "--> %lu %lu %f %f %f\n", is, iM, fiM, fata[is], fM );
-                                //if (is % 100 == 0) getchar();
-                                p_average_signal->Fill(is - (fiM - 450), fata[is] / fM);
-                        }
-                }
-*/
                 ++totpulse;
         }
         ofs.close();
