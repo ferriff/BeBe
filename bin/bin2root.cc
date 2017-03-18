b7f32884c808292a928adbef18b65421d014642a
Federico Ferri <federico.ferri@cern.ch>
2017-03-18 08:49:46 +0100

    add support for changing the number of samples of a pulse

--- bin/bin2root.cc
+++ bin/bin2root.cc
@@ -15,6 +15,8 @@ void help(const char * progr)
         "-H <file>, --heat-file=<file>             trigger mode: input binary file, heat channel\n"
         "-L <file>, --light-file=<file>            trigger mode: input binary file, light channel\n"
         "-T <file>, --trigger-file=<file>          trigger mode: input text file, trigger information\n"
+        //"-S <file>, --streamer-file=<file>         streamer mode: input binary file\n"
+        "-s <nsamples>, --sample-number=<int>      number of samples for each pulse\n"
         "\n"
         "Converts binary files into pre-processed root files.\n"
         "The default is to read streamer-mode binary files [file(s)].\n"
@@ -33,16 +35,16 @@ int main(int argc, char ** argv)
                 {"heat-file"    , required_argument, 0, 'H'} , 
                 {"light-file"   , required_argument, 0, 'L'} , 
                 {"trigger-file" , required_argument, 0, 'T'} , 
-                {"streamer-file", required_argument, 0, 'S'} , 
+                //{"streamer-file", required_argument, 0, 'S'} , 
                 {"help"         , no_argument,       0, 'h'},
                 {0, 0, 0, 0}
         };
         /* getopt_long stores the option index here. */
         int option_index = 0;
-        int c = 0;
-        std::string ofile = "output.root", fheat, flight, ftrigger;
+        int c = 0, nsamples = 0;
+        std::string ofile = "output.root", fheat, flight, ftrigger, fstreamer;
         int cnt = 0;
-        while ((c = getopt_long(argc, argv, "o:H:L:T:S:h", long_options, &option_index)) != -1)
+        while ((c = getopt_long(argc, argv, "o:H:L:T:S:s:h", long_options, &option_index)) != -1)
         {
                 switch(c) {
                         case 'h':
@@ -63,25 +65,28 @@ int main(int argc, char ** argv)
                                 ftrigger = optarg;
                                 ++cnt;
                                 break;
+                        case 's':
+                                nsamples = atoi(optarg);
+                                break;
                         default:
                                 help(argv[0]);
                                 break;
                 }
         }
+        bb::data_reader dr(ofile.c_str());
+        if (nsamples) dr.set_n_samples(nsamples);
         if (cnt) {
                 // trigger-mode binary file
                 if (cnt != 3) {
                         std::cerr << "Error: Either all or none of -H, -T, -L must be specified.\n";
                         exit(-1);
                 } else {
-                        bb::data_reader dr(ofile.c_str());
                         dr.read_trigger_mode_file(fheat.c_str(), flight.c_str(), ftrigger.c_str());
                 }
         } else {
                 // streamer-mode binary file
-                bb::data_reader dr(ofile.c_str());
                 for (int i = optind; i < argc; ++i) {
-                        dr.read_streamer_mode_file(argv[i]);
+                        dr.read_streamer_mode_file(argv[1]);
                 }
         }
         return 0;
