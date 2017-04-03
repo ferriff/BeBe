c1e7377b0f2440f36c4b2369454fde94b1c6a33a
Federico Ferri <federico.ferri@cern.ch>
2017-04-04 00:17:10 +0200

    support multiple trees for streaming-mode data

--- include/data_reader.h
+++ include/data_reader.h
@@ -30,9 +30,11 @@ namespace bb {
 
                         void init_tree(const char * name);
                         void add_intra_tree_info();
+                        void add_meta_data_info();
                         void copy_data(size_t idx, daqint_t * d1, daqint_t * d2);
 
                         bool trigger(daqint_t * data, size_t idx);
+                        bool trigger_derivative(daqint_t * data, size_t idx);
                         bool trigger_over_threshold(daqint_t * data, size_t idx);
                         bool trigger_over_threshold_with_baseline(daqint_t * data, size_t idx);
 
@@ -44,6 +46,7 @@ namespace bb {
                         size_t _ndetids = 0;
                         size_t _cnt = 0;
                         int32_t _detid;
+                        std::map<std::string, int32_t> _detid_names;
                         Double_t _time;
                         Double_t _freq;
                         Long64_t * _event_ids;
--- src/data_reader.cc
+++ src/data_reader.cc
@@ -53,6 +53,23 @@ void bb::data_reader::add_intra_tree_info()
         }
 }
 
+void bb::data_reader::add_meta_data_info()
+{
+        size_t cnt = 0;
+        std::vector<std::string> tnames;
+        for (auto t : _t) {
+                tnames.push_back(t->GetName());
+        }
+        auto dir = gDirectory->GetPath();
+        _fout->cd("MetaData");
+        TTree * meta_data = new TTree("meta", "meta");
+        meta_data->Branch("tree_order", &tnames);
+        meta_data->Fill();
+        meta_data->Write();
+        delete meta_data;
+        gDirectory->cd(dir);
+}
+
 
 void bb::data_reader::copy_data(size_t idx, daqint_t * d1, daqint_t * d2)
 {
@@ -92,9 +109,16 @@ bool bb::data_reader::trigger_over_threshold_with_baseline(daqint_t * data, size
 }
 
 
+bool bb::data_reader::trigger_derivative(daqint_t * data, size_t idx)
+{
+        return data[(idx + 25) % _nsamples] - data[idx] > 500;
+}
+
+
 bool bb::data_reader::trigger(daqint_t * data, size_t idx)
 {
-        trigger_over_threshold_with_baseline(data, idx);
+        //trigger_over_threshold_with_baseline(data, idx);
+        trigger_derivative(data, idx);
 }
 
 
@@ -112,7 +136,7 @@ void bb::data_reader::read_streamer_mode_file(const char * input_file_name)
 
         // read and parse header
         std::vector<int> detids;
-        std::vector<std::string> detid_names;
+        std::vector<std::string> dnames;
         std::string header = "";
         char dname[128];
         _freq = 0;
@@ -145,9 +169,23 @@ void bb::data_reader::read_streamer_mode_file(const char * input_file_name)
                 fprintf(stderr, "[bb::data_reader::read_streamer_mode_file] ... found %lu detectors instead of %lu, aborting.\n", detids.size(), _ndetids);
                 return;
         }
+
+        // create association  detector name <-> detid
+        if (_first_file) {
+                for (size_t i = 0; i < dnames.size(); ++i) {
+                        fprintf(stderr, "--> %s %d\n", dnames[i].c_str(), detids[i]);
+                        //_detid_names.push_back(std::make_pair(dnames[i], detids[i]));
+                        _detid_names[dnames[i]] = detids[i];
+                }
+        }
+        // FIXME: add more checks on detid ordering when switching files...
+
         _ndetids = detids.size();
         _event_ids = (Long64_t *)calloc(_ndetids, sizeof(Long64_t));
-        if (_first_file) add_intra_tree_info();
+        if (_first_file) {
+                add_intra_tree_info();
+                add_meta_data_info();
+        }
 
         // write header information in the output file
         _fout->cd("MetaData");
@@ -170,7 +208,7 @@ void bb::data_reader::read_streamer_mode_file(const char * input_file_name)
                 data[i] = (daqint_t *)calloc(_nsamples, sizeof(daqint_t));
         }
         daqint_t cdata[_nsamples];
-        daqint_t sample;
+        daqint_t sample[_ndetids];
 
         // read data and fill the ntuple
         //  1)  fill an array of data
@@ -182,91 +220,71 @@ void bb::data_reader::read_streamer_mode_file(const char * input_file_name)
         //        . write the event in the corresponding tree
         //        . write the tree indices of the previous events of all the
         //          other trees (to ease navigation)
-        size_t ntot = _ndetids * _nsamples;
+        //size_t ntot = _ndetids * _nsamples;
 
         // to hold the last triggered sample
         Long64_t trg[_ndetids];
         for (auto i = 0; i < _ndetids; ++i) trg[i] = 0;
 
         // read an array of ndetids_
-        while (fread(&sample, sizeof(daqint_t), 1, fd)) {
+        while (fread(&sample, _ndetids * sizeof(daqint_t), 1, fd)) {
                 // start looking for triggers when the array is fully filled
                 // from 1/3 of the array, i.e. the first 1/3 is kept for
                 // the pre-samples
-                if (_cnt > ntot) {
-                        // if sample above trigger threshold
-                        // and outside the trigger window, save the event
-                        // in the tree of the corresponding detid
-                        size_t idetid = _cnt % _ndetids;
-                        //if (idetid == 10) {
-                                fprintf(stderr, "%d %c\n", sample, (char)sample);
-                        //}
-                        // divide _nsamples in 3 equal parts:
-                        // 1. pre-samples (no triggers searched for)
-                        // 2. one and only one trigger allowed
-                        // 3. post-samples (new triggers allowed)
-                        size_t pre_samples = _nsamples / 3;
-                        size_t core_samples = _nsamples / 3;
-                        // start looking for triggers from idx
-                        size_t idx = (_cnt / _ndetids + pre_samples) % _nsamples;
-                        if (idetid == 10 && trigger(data[idetid], idx) && (_cnt / _ndetids - trg[idetid]) > core_samples) {
-                                fprintf(stderr, "# Copying data: %lu --> %lu %lu    %d  %lu %lld\n", _cnt, idetid, idx, sample, _cnt / _ndetids, trg[idetid]);
-                                trg[idetid] = _cnt / _ndetids;
-                                int start = (idx - pre_samples);
-                                bzero(cdata, sizeof(cdata));
-                                copy_data(start > 0 ? start : _nsamples + start, data[idetid], cdata);
-                                //if (idetid == 6) {
-                                //        for (size_t i = 0; i < _nsamples; ++i)
-                                //        {
-                                //                fprintf(stderr, "%lu %d %d\n", i, data[idetid][i], cdata[i]);
-                                //        }
-                                //        fprintf(stderr, "\n\n");
-                                //}
-                                _br[idetid]->SetAddress(cdata);
-                                _detid = detids[idetid];
-                                _t[idetid]->Fill();
-                                ++_event_ids[idetid];
+                /////////////fprintf(stderr, "%d  ", nb);
+                /////////////for (size_t i = 0; i < _ndetids; ++i) fprintf(stderr, " %10d", sample[i]);
+                /////////////fprintf(stderr, "\n");
+                for (size_t idetid = 0; idetid < _ndetids; ++idetid) {
+                        if (_cnt > _nsamples) {
+                                // if sample above trigger threshold
+                                // and outside the trigger window, save the event
+                                // in the tree of the corresponding detid
+                                //size_t idetid = _cnt % _ndetids;
+                                // divide _nsamples in 3 equal parts:
+                                // 1. pre-samples (no triggers searched for)
+                                // 2. one and only one trigger allowed
+                                // 3. post-samples (new triggers allowed)
+                                size_t pre_samples = _nsamples / 3;
+                                size_t core_samples = _nsamples / 3;
+                                // start looking for triggers from idx
+                                size_t idx = (_cnt + pre_samples) % _nsamples;
+                                if (trigger(data[idetid], idx) && (_cnt - trg[idetid]) > core_samples) {
+                                        fprintf(stderr, "# Copying data: %lu --> %lu %lu    %d  %lu %lld\n", _cnt, idetid, idx, sample, _cnt / _ndetids, trg[idetid]);
+                                        trg[idetid] = _cnt;
+                                        int start = (idx - pre_samples);
+                                        bzero(cdata, sizeof(cdata));
+                                        copy_data(start > 0 ? start : _nsamples + start, data[idetid], cdata);
+                                        //if (idetid == 6) {
+                                        //        for (size_t i = 0; i < _nsamples; ++i)
+                                        //        {
+                                        //                fprintf(stderr, "%lu %d %d\n", i, data[idetid][i], cdata[i]);
+                                        //        }
+                                        //        fprintf(stderr, "\n\n");
+                                        //}
+                                        _br[idetid]->SetAddress(cdata);
+                                        _detid = detids[idetid];
+                                        _t[idetid]->Fill();
+                                        ++_event_ids[idetid];
+                                }
+                                //// keep track of the time
+                                //_time += (Double_t)_nsamples / _freq;
                         }
-                        //// keep track of the time
-                        //_time += (Double_t)_nsamples / _freq;
+                        // keep track of the time
+                        _time = (Double_t)(_cnt) / _freq;
+                        // fill the data array
+                        data[idetid][_cnt % _nsamples] = sample[idetid];
                 }
-                // keep track of the time
-                _time = (Double_t)(_cnt / _ndetids) / _freq;
-                // fill the data array
-                data[_cnt % _ndetids][(_cnt / _ndetids) % _nsamples] = sample;
                 ++_cnt;
         }
         for (size_t i = 0; i < _ndetids; ++i) {
                 free(data[i]);
         }
-
-        /*
-        // read data and fill the ntuple
-        daqint_t sample;
-        int _cnt = 0;
-        while (fread(&sample, sizeof(daqint_t), 1, fd)) {
-                if (_cnt / (_ndetids * _nsamples) && _cnt % (_ndetids * _nsamples) == 0) {
-                        for (size_t i = 0; i < _ndetids; ++i) {
-                                _br->SetAddress(data[i]);
-                                _detid = detids[i];
-                                _t->Fill();
-                        }
-                        _time += (Double_t)_nsamples / _freq;
-                }
-                data[_cnt % _ndetids][(_cnt / _ndetids) % _nsamples] = sample;
-                ++_cnt;
-        }
-        for (size_t i = 0; i < _ndetids; ++i) {
-                free(data[i]);
-        }
-        */
         _first_file = false;
 }
 
 
 void bb::data_reader::read_trigger_mode_file(const char * heat_data_file, const char * light_data_file, const char * trigger_file)
 {
-        /*
         FILE * fd_heat = fopen(heat_data_file, "r");
         if (!fd_heat) {
                 fprintf(stderr, "[bb::data_reader::read_trigger_mode_file] Cannot open file `%s'. Abort.\n", heat_data_file);
@@ -284,9 +302,11 @@ void bb::data_reader::read_trigger_mode_file(const char * heat_data_file, const 
         }
         fprintf(stderr, "[bb::data_reader::read_trigger_mode_file] Number of samples set to %lu. Make sure it is the correct one for the file read.\n", _nsamples);
 
+        init_tree("ntu");
+
         daqint_t * data;
         data = (daqint_t *)calloc(_nsamples, sizeof(daqint_t));
-        _br->SetAddress(data);
+        _br[0]->SetAddress(data);
         daqint_t sample;
 
         char * line = NULL;
@@ -303,7 +323,7 @@ void bb::data_reader::read_trigger_mode_file(const char * heat_data_file, const 
                         //sample = bswap(sample);
                         data[i] = sample;
                 }
-                _t->Fill();
+                _t[0]->Fill();
                 // read and fill light
                 _detid += 1000;
                 for (size_t i = 0; i < _nsamples; ++i) {
@@ -311,15 +331,14 @@ void bb::data_reader::read_trigger_mode_file(const char * heat_data_file, const 
                         //sample = bswap(sample);
                         data[i] = sample;
                 }
-                _t->Fill();
+                _t[0]->Fill();
                 ++is;
         }
         if (data) free(data);
         if (fread(&sample, sizeof(daqint_t), 1, fd_heat) != 0 || fread(&sample, sizeof(daqint_t), 1, fd_light) != 0) {
                 fprintf(stderr, "[bb::data_reader::read_trigger_mode_file] ERROR: data"
-                                " files seem to contain more events than"
-                                " trigger file, abort\n");
+                        " files seem to contain more events than"
+                        " trigger file, abort\n");
                 exit(1);
         }
-        */
 }
