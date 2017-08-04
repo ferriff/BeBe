c4b75b51dc2663d93ebf28fd1c1209badc078dcc
Federico Ferri <federico.ferri@cern.ch>
2017-08-04 12:53:06 +0200

    example of tree reading with navigation

--- bin/player.cc
+++ bin/player.cc
@@ -10,18 +10,40 @@ void print_pulse(bb::event & e)
         }
 }
 
-
 int main()
 {
-        bb::tree_reader tr("pappa_derivative.root");
-        tr.set_detector(6);
+        //bb::tree_reader tr("pappa_derivative.root");
+        bb::tree_reader tr("test2.root");
+        tr.set_detector("LMO1t_chaleur");
+        auto curr_det = tr.detector();
+        fprintf(stderr, "_ct = %lu -> %s\n", tr.detector(), tr.detector_name().c_str());
+        //tr.set_detector("CWO_chaleur");
         size_t cnt = 0;
+        Long64_t * ids = (Long64_t*)malloc(sizeof(Long64_t) * tr.n_detectors());
         while (tr.next_event()) {
-                fprintf(stderr, "--> %f\n", tr.e().time());
-                print_pulse(tr.e());
-                for (int i = 0; i < 12; ++i) {
+                auto t = tr.e().time();
+                // copy to avoid that changing the detector points to a different set of ids
+                memcpy(ids, tr.e().event_ids(), sizeof(Long64_t) * tr.n_detectors());
+                fprintf(stderr, "--> %f   ", t);
+                for (size_t i = 0; i < tr.n_detectors(); ++i) {
+                        fprintf(stderr, " %2lu:%lld", i, ids[i]);
                 }
-                break;
+                fprintf(stderr, "\n");
+                //print_pulse(tr.e());
+                for (size_t i = 0; i < tr.n_detectors(); ++i) {
+                        tr.set_detector(i);
+                        auto ret_prev = tr.read_event(ids[i]);
+                        auto t_prev = tr.e().time();
+                        if (ret_prev == 0) t_prev = -1;
+                        auto ret_next = tr.read_event(ids[i] + 1);
+                        auto t_next = tr.e().time();
+                        if (ret_next == 0) t_next = -1;
+                        fprintf(stderr, ":=: %2lu %15s  %+9.4f %8lld    %+9.4f    %2d %2d\n", i, tr.detector_name().c_str(), t_prev, ids[i], t_next, ret_prev, ret_next);
+                }
+                tr.set_detector(curr_det);
+                ++cnt;
         }
+        free(ids);
+        fprintf(stderr, "end> %f\n", tr.e().time());
         return 0;
 }
